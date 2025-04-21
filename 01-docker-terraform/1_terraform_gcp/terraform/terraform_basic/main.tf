65162bc47d8217f9a9e04a1b38a8ce4dea0b1c58
Nia Ulan Sari <71682884+niaulans@users.noreply.github.com>
2025-04-21 10:20:16 +0000

    add terraform files

---
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "nodal-pod-448911-c5"
+  region  = "asia-southeast1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "nodal-pod-448911-c5-terra-bucket"
+  location      = "ASIA-SOUTHEAST1"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = "demo_dataset"
+}
---
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
@@ -0,0 +1,35 @@
+variable "credentials" {
+  description = "My GCP Credentials"
+  default     = "../../../../creds/terraform-key.json"
+}
+
+variable "project" {
+  description = "Project"
+  default     = "nodal-pod-448911-c5"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "asia-southeast1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "ASIA-SOUTHEAST1"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "nodal-pod-448911-c5-terra-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
+
